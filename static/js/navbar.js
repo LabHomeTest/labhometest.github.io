34b0993fda5f685d31bd1860c75c4217db8d435b
Tania-Zhang <578049528@qq.com>
2024-04-30 14:30:09 +0800

    修改导航栏初始化的问题

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var curHrefList = window.location.href.split('/');
-
+    var hasActive = 0 ;
     //导航栏
     var navs = ["index", "Publications", "Projects", "Team"];
     // 定位到头部导航栏容器
@@ -9,12 +9,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // 创建头部导航栏元素
     var navbarElement = document.createElement('div');
     var myHtml = ' <ul class="navbar">';
+    
     for(var i = 0;i<navs.length;i++){
         var pageName = navs[i];
         if(navs[i] == "index"){
             pageName = "Home";
         }
+        
         if(curHrefList[curHrefList.length-1] == navs[i]+'.html'){
+            hasActive = 1;
             myHtml = myHtml + '<li><a href="' + navs[i] +'.html" class = "active">'+ pageName +'</a></li>';
         }else{
             myHtml = myHtml + '<li><a href="' + navs[i] +'.html">'+ pageName +'</a></li>';
@@ -31,35 +34,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // 为每个导航栏项注册点击事件处理程序
     for (var i = 0; i < navItems.length; i++) {
+        if(!hasActive && i==0){
+            navItems[0].classList.add("active");
+        }
         navItems[i].addEventListener('click', function(event) {
             // 取消默认行为
             event.preventDefault();
+            hasActive = 1;
 
-            //$(".navbar li a").removeClass("active");
-            //$(this).addClass("active");
-            // 移除当前选中样式
-            // var selectedNavItem = navbarContainer.querySelector('.active');
-            // if (selectedNavItem) {
-            //     selectedNavItem.classList.remove('active');
-            // }
-
-            // 添加当前导航栏项的选中样式
-            //this.classList.add('active');
             // 获取导航栏项的链接地址并重定向页面
             var href = this.getAttribute('href');
             window.location.href = href;
         });
     }
-
-    // $(document).ready(function(){
-    //     $(".navbar li a").click(function(){
-    //         $(".navbar li a").removeClass("active");
-    //         $(this).addClass("active");
-    //         // var navNum = $(this).attr("pageNum");
-    //         // $("#pageContent").children().attr("src", navs[navNum]);
-    //     });
-        
-    // });
-        
+      
 });
-
